0c0ad382ccef1dae56ce73f8b582e76b2f473c1c
kevd82 <80932709+kevd82@users.noreply.github.com>
2021-06-07 18:33:59 -0700

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+print("Hello World")
+name = "Kevin"
+print("Hello", name)
+name = "Kevin"
+print("Hello" + name)
+favNumber = 22
+print("Hello", favNumber)
+fav = "22"
+print("Hello" + fav)
+favFood1 = "Mashed Potatoes"
+favFood2 = "Peanut Butter"
+print("I love to eat {} and {}.".format(favFood1, favFood2))
+print(f"I love to eat {favFood1} and {favFood2}.")
