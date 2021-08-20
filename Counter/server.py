55668b28bc04d69c1582046b390c550835462d42
kevd82 <80932709+kevd82@users.noreply.github.com>
2021-08-19 17:27:18 -0700

    Add files via upload

---
+++ Counter/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, session
+app=Flask(__name__)
+app.secret_key= "iceni"
+
+@app.route("/")
+def counter():
+    if "count" not in session:
+        session["count"] = 0
+    else:
+        session["count"] += 1
+    return render_template("index.html")
+
+@app.route("/counter2")
+def counter2():
+    if "count" not in session:
+        session["count"] = 0
+    else:
+        session["count"] += 2
+    return render_template("index.html")
+
+@app.route("/reset")
+def reset():
+    session.clear()
+    return redirect("/")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
