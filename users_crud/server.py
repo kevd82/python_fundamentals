b04dd7c6cdf85e837b7b2f1099be8d4ea22ab56d
kevd82 <80932709+kevd82@users.noreply.github.com>
2021-09-02 18:45:53 -0700

    Add files via upload

---
+++ users_crud/server.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, redirect
+from user import User
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return redirect("/Read(All)")
+
+@app.route("/Read(All)")
+def show_all():
+    return render_template("Read(All).html", users=User.get_all())
+
+@app.route("/Read(One)/<int:id>")
+def show_one(id):
+    data = {
+        "id":id
+    }
+    return render_template("Read(One).html", user=User.get_one(data))
+
+@app.route("/Edit/<int:id>")
+def edit(id):
+    data ={
+        "id":id
+    }
+    return render_template("Edit.html", user=User.get_one(data))
+
+@app.route("/Update", methods=["POST"])
+def update():
+    User.update(request.form)
+    return redirect("/Read(All)")
+
+@app.route("/Delete/<int:id>")
+def delete(id):
+    data ={
+        "id":id
+        }
+    User.delete(data)
+    return redirect("/Read(All)")
+
+@app.route("/Add")
+def add():
+    return render_template("/Create.html")
+
+@app.route("/Create", methods=["POST"])
+def create_user():
+    User.save(request.form)
+    return redirect('/Read(All)')
+
+if __name__ == "__main__":
+    app.run(debug=True)
